40ef252b65254fe645f9bbcbed502deac67873e9
risenagata <okkiy0526@gmail.com>
2026-07-26 13:41:46 +0900

    パスワード変更実装

--- app/setting/password/page.tsx
+++ app/setting/password/page.tsx
@@ -1,31 +1,25 @@
 //パスワード変更画面
 
-import Button from "@/shared/components/Button";
-import Input from "@/shared/components/Input";
-import Label from "@/shared/components/Label";
-import PageTitle from "@/shared/components/PageTitle";
 
-export default function SettingPassword(){
+import PageTitle from "@/shared/components/PageTitle";
+import SettingPasswordForm from "./SettingPasswordForm";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export default async function SettingPassword(){
+
+        const supabase=await createClient()
+        const { data: { user } } = await supabase.auth.getUser();
+        if(!user){
+            redirect("/auth/signin")
+        }
+
     return(
         <div className="m-5 flex flex-col items-center">
             <PageTitle>パスワード変更</PageTitle>
-            <form className="w-full max-w-md">
-                <div className="py-6">
-                    <Label>新しいパスワード</Label>
-                    <Input type="password" placeholder="英数字6文字以上" className="w-full"/>
-                </div>
-                <div className="py-6">
-                    <Label>新しいパスワード（確認用）</Label>
-                    <Input type="password" placeholder="英数字6文字以上" className="w-full"/>
-                </div>
 
-                
-                <div className="flex flex-col justify-center items-center gap-4 pt-6">
-                    <Button color="primary" className="w-full">保存</Button>
-                    <Button color="secondary" className="w-full">キャンセル</Button>
-                </div>
+                <SettingPasswordForm />
 
-            </form>
         </div>
     )
 }
