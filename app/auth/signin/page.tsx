b838f2f2cbeffdfea9cbaf59564886afac8255cd
risenagata <okkiy0526@gmail.com>
2026-08-13 17:45:28 +0900

    SNSログイン・サインアップボタン　コメントアウト

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,9 +1,9 @@
 //ログイン画面
 
-import Button from "@/shared/components/Button";
+// import Button from "@/shared/components/Button";
 import PageTitle from "@/shared/components/PageTitle";
-import { FaXTwitter } from "react-icons/fa6";
-import { FcGoogle } from "react-icons/fc";
+// import { FaXTwitter } from "react-icons/fa6";
+// import { FcGoogle } from "react-icons/fc";
 import LoginForm from "./LoginForm";
 
 export default function Signin(){
@@ -16,18 +16,18 @@ export default function Signin(){
                 <LoginForm />
             
 
-            <div className="flex items-center w-full max-w-md my-5">
-            <div className="flex-1 border-b border-gray-300" />
-            <span className="mx-4 text-sm text-gray-500">
-                または
-            </span>
-            <div className="flex-1 border-b border-gray-300" />
+            {/* <div className="flex items-center w-full max-w-md my-5">
+                <div className="flex-1 border-b border-gray-300" />
+                <span className="mx-4 text-sm text-gray-500">
+                    または
+                </span>
+                <div className="flex-1 border-b border-gray-300" />
             </div>
 
             <div className="flex flex-col gap-5 items-center">
                 <Button color="others"><FaXTwitter/>X(Twitter)でログイン</Button>
                 <Button color="others"><FcGoogle/>Googleでログイン</Button>
-            </div>
+            </div> */}
             
     
         </div>
--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,9 @@
 //新規登録画面
 
-import Button from "@/shared/components/Button";
+// import Button from "@/shared/components/Button";
 import PageTitle from "@/shared/components/PageTitle";
-import { FaXTwitter } from "react-icons/fa6";
-import { FcGoogle } from "react-icons/fc";
+// import { FaXTwitter } from "react-icons/fa6";
+// import { FcGoogle } from "react-icons/fc";
 import SignupForm from "./SignupForm";
 
 export default function Signup(){
@@ -16,7 +16,7 @@ export default function Signup(){
                 <SignupForm />
             
 
-            <div className="flex items-center w-full max-w-md my-5">
+            {/* <div className="flex items-center w-full max-w-md my-5">
             <div className="flex-1 border-b border-gray-300" />
             <span className="mx-4 text-sm text-gray-500">
                 または
@@ -27,7 +27,7 @@ export default function Signup(){
             <div className="flex flex-col gap-5 items-center">
                 <Button color="others"><FaXTwitter/>X(Twitter)でアカウント作成</Button>
                 <Button color="others"><FcGoogle/>Googleでアカウント作成</Button>
-            </div>
+            </div> */}
             
     
         </div>
