00995f8b72f21d10e391ef06cdff3fe4656979c2
risenagata <okkiy0526@gmail.com>
2026-08-24 21:16:54 +0900

    不要なコメントアウトの削除

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,9 +1,7 @@
 //新規登録画面
 
-// import Button from "@/shared/components/Button";
+
 import PageTitle from "@/shared/components/PageTitle";
-// import { FaXTwitter } from "react-icons/fa6";
-// import { FcGoogle } from "react-icons/fc";
 import SignupForm from "./SignupForm";
 
 export default function Signup(){
