61e4f648d37ea076bc514a9351c7b8dc0d0e388b
risenagata <okkiy0526@gmail.com>
2026-08-24 21:17:53 +0900

    不要なコメントアウトの削除

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,9 +1,7 @@
 //ログイン画面
 
-// import Button from "@/shared/components/Button";
+
 import PageTitle from "@/shared/components/PageTitle";
-// import { FaXTwitter } from "react-icons/fa6";
-// import { FcGoogle } from "react-icons/fc";
 import LoginForm from "./LoginForm";
 import Link from "next/link";
 
@@ -22,26 +20,9 @@ export default function Signin(){
                     <p className="text-gray-500 text-lg hover:text-gray-700">＜パスワードを忘れた方はこちら＞</p>                
                 </Link>
             </div>
-            
 
-            {/* <div className="flex items-center w-full max-w-md my-5">
-                <div className="flex-1 border-b border-gray-300" />
-                <span className="mx-4 text-sm text-gray-500">
-                    または
-                </span>
-                <div className="flex-1 border-b border-gray-300" />
-            </div>
-
-            <div className="flex flex-col gap-5 items-center">
-                <Button color="others"><FaXTwitter/>X(Twitter)でログイン</Button>
-                <Button color="others"><FcGoogle/>Googleでログイン</Button>
-            </div> */}
             
-    
         </div>
-            
-
-
-        
+                    
     )
 }
